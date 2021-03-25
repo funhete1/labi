37ed09162bf519e7f41acd37d70d9a81c8f27fe2
funhete1 <51arthurm@gmail.com>
2021-03-25 20:40:15 +0000

    vai te fuder

--- guiao/semana12/1215.py
+++ guiao/semana12/1215.py
@@ -1,9 +1,9 @@
 frase = input("Diz a frase ai filhao: ")
+
 array = []
 
-for i in range(0, len(frase) - 1):
-    array[i] = frase.split(" ")
 
 for i in range(0, len(frase) - 1):
-    print(array[i])
-    
+    array[]
+
+print(counter)
---
+++ guiao/semana12/1216.py
@@ -0,0 +1,4 @@
+s = input("Qual frase tu quer inverter ?\n")
+
+for i in range(0, len(s)):
+    print(s[(len(s) - 1) - i])
