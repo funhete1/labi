8ded0bee8b0faad06174b610fb831b0035de4f3f
funhete1 <51arthurm@gmail.com>
2021-03-27 03:32:34 +0000

    .

--- guiao/semana12/1220.py
+++ guiao/semana12/1220.py
@@ -1,8 +1,20 @@
 from funcao import words_count
+import os.path
+import sys
+
+
+fname = "1220.txt"
+
+if not os.path.exists(fname):
+    sys.exit("Não existe")
+if os.path.isdir(fname):
+    sys.exit("É diretório")
+if not os.path.isfile(fname):
+    sys.exit("Não é ficheiro")
 
 characters, words, lines = 0, 0, 0
 
-f = open("1220.txt", "r")
+f = open(fname, "r")
 
 for linha in f:
     characters+= len(linha)
--- guiao/semana12/1221.py
+++ guiao/semana12/1221.py
@@ -1,6 +1,20 @@
 from funcao import reverse
+import os.path
+import sys
 
-f = open("1221.txt", "r")
+
+fname = "1221.txt"
+
+if not os.path.exists(fname):
+    sys.exit("Não existe")
+if os.path.isdir(fname):
+    sys.exit("É diretório")
+if not os.path.isfile(fname):
+    sys.exit("Não é ficheiro")
+
+f = open(fname,"r")
 
 for linha in f:
     print(reverse(linha)) #da merda se tiver mais linhas no txt
+
+f.close()
