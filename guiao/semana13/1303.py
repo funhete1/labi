9612dd8782285b2d483bd5f783acc0d0b22ab99e
funhete1 <51arthurm@gmail.com>
2021-03-28 23:53:23 +0100

    .

---
+++ guiao/semana13/1303.py
@@ -0,0 +1,43 @@
+from Crypto.Hash import SHA256
+import sys
+import os
+
+h = SHA256.new()
+
+entext = ""
+iname = sys.argv[1]
+out = sys.argv[2]
+
+if not os.path.exists(iname):
+    sys.exit("Não existe")
+    exit
+if os.path.isdir(iname):
+    sys.exit("É diretório")
+    exit
+if not os.path.isfile(iname):
+    sys.exit("Não é ficheiro")
+    exit
+if (os.stat(iname).st_size == 0):
+    print("File is empty!")
+    sys.exit()
+
+if not os.path.exists(out):
+    sys.exit("Não existe")
+    exit
+if os.path.isdir(out):
+    sys.exit("É diretório")
+    exit
+if not os.path.isfile(out):
+    sys.exit("Não é ficheiro")
+    exit
+
+f = open(iname, "rb")
+entext = f.read(512)
+while len(entext) != 0:
+    h.update(entext)
+    entext = f.read(512)
+f.close()
+
+f = open(out, "w")
+f.write(h.hexdigest())
+f.close()
